0987be54c28c7e1e66041ea86e04bef523e6bd27
Angelo Ortega <angeloale1@gmail.com>
2020-06-23 19:33:35 -0600

    Updates

---
+++ precache-manifest.00b1a75679693bf9c60a99303a80b66e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c97a5d4754c1ac6d321728fc202eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0e3bcf9ba77fcc584de",
+    "url": "/static/css/main.3109c4d6.chunk.css"
+  },
+  {
+    "revision": "d07ea773c9493b76dae7",
+    "url": "/static/js/2.6fd59d97.chunk.js"
+  },
+  {
+    "revision": "16a4527db387cf31fd19131d298aca07",
+    "url": "/static/js/2.6fd59d97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0e3bcf9ba77fcc584de",
+    "url": "/static/js/main.a6a419ab.chunk.js"
+  },
+  {
+    "revision": "98329d749cb35261e204",
+    "url": "/static/js/runtime-main.6a0d7e7d.js"
+  },
+  {
+    "revision": "dbf7907c27c1e880883ec1bca800b171",
+    "url": "/static/media/logo.dbf7907c.png"
+  },
+  {
+    "revision": "d71d7d138256e7a1e93d8b973e8e0711",
+    "url": "/static/media/logoBN.d71d7d13.png"
+  },
+  {
+    "revision": "541b85950f312daae3d35d24a5a49311",
+    "url": "/static/media/logout.541b8595.png"
+  },
+  {
+    "revision": "611ea5352904228e1ca4dc84fe0687d1",
+    "url": "/static/media/nuevaReceta.611ea535.png"
+  }
+]);
